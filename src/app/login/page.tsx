01ed50fb03303bd87d2f86684a7c0430bf7471b8
Veerasiva <veerasiva01@gmail.com>
2025-01-18 12:27:06 +0530

    add profile and params setup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,56 @@
-export default function Login() {
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function LoginPage() {
+   const [user, setUser] = useState({
+      email: "",
+      password: "",
+   });
+
+   const onLogin = async () => {
+      
+   };
+
+
    return (
-      <div>
+      <div className="flex flex-col items-center justify-center py-2 min-h-screen gap-y-1">
          <h1 className="text-3xl text-center">Login</h1>
+         <label htmlFor="email">email</label>
+         <hr className="h-2" />
+         <input
+            className="p-1 rounded-lg"
+            type="text"
+            id="email"
+            value={user.email}
+            onChange={(e) => setUser({...user, email: e.target.value})}
+            placeholder="email"
+
+         />
+         <label htmlFor="password">password</label>
+         <input
+            className="p-1 rounded-lg"
+            type="password"
+            id="password"
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+
+         />
+         <button
+            className="border"
+            onClick={onLogin}
+
+         >Login here
+         </button>
+         <Link href="/signup">Go to sign-up page</Link>
+
+
+
+
+
+
       </div>
    )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export default function Signup() {
+export default function SignupPage() {
    const [user, setUser] = useState({
       username: "",
       email: "",
@@ -15,8 +16,51 @@ export default function Signup() {
 
 
    return (
-      <div>
+      <div className="flex flex-col items-center justify-center py-2 min-h-screen gap-y-1">
          <h1 className="text-3xl text-center">Sign-up</h1>
+         <label htmlFor="username">username</label>
+         <input
+            className="p-1 rounded-lg"
+            type="text"
+            id="username"
+            value={user.username}
+            onChange={(e) => setUser({...user, username: e.target.value})}
+            placeholder="Username"
+
+         />
+         <label htmlFor="email">email</label>
+         <input
+            className="p-1 rounded-lg"
+            type="text"
+            id="email"
+            value={user.email}
+            onChange={(e) => setUser({...user, email: e.target.value})}
+            placeholder="email"
+
+         />
+         <label htmlFor="password">password</label>
+         <input
+            className="p-1 rounded-lg"
+            type="password"
+            id="password"
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+
+         />
+         <button
+            className="border"
+            onClick={onSignup}
+
+         >Signup here
+         </button>
+         <Link href="/login">Go to login page</Link>
+
+
+
+
+
+
       </div>
    )
 }
