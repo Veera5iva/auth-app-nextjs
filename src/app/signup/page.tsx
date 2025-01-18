d4d61af4f90afeff8ed834b95452d7227becfe2a
Veerasiva <veerasiva01@gmail.com>
2025-01-18 23:43:43 +0530

    place api folder inside the app and update signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,36 +1,65 @@
 "use client";
+import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function SignupPage() {
+   const router = useRouter();
+
    const [user, setUser] = useState({
       username: "",
       email: "",
       password: "",
    });
+   const [buttonDisabled, setButtonDisabled] = useState(true);
+   const [loading, setLoading] = useState(false);
+
+   useEffect(() => {
+      if(user.username.trim().length > 0 && user.email.trim().length > 0 && user.password.trim().length > 0) {
+         setButtonDisabled(false);
+      }
+      else setButtonDisabled(true);
+   }, [user])
 
    const onSignup = async () => {
+      try {
+         setLoading(true);
+         const response = await axios.post("/api/users/signup", user)
+         console.log("User signed up successfully", response.data);
+         router.push("/login")
+         
+      } catch (error : unknown) {
+
+         console.log("Signup failed", error);
+         
+         if (error instanceof Error ) toast.error(error.message);
+         
+      } finally {
+         setLoading(false);
+      }
       
    };
 
 
    return (
       <div className="flex flex-col items-center justify-center py-2 min-h-screen gap-y-1">
-         <h1 className="text-3xl text-center">Sign-up</h1>
+         <Toaster position="top-right" reverseOrder={false}/>
+         <h1 className="text-3xl text-center">{loading ? "Processing" : "Signup"}</h1>
          <label htmlFor="username">username</label>
          <input
-            className="p-1 rounded-lg"
+            className="p-2 rounded-md text-black"
             type="text"
             id="username"
             value={user.username}
             onChange={(e) => setUser({...user, username: e.target.value})}
-            placeholder="Username"
+            placeholder="username"
 
          />
          <label htmlFor="email">email</label>
          <input
-            className="p-1 rounded-lg"
+            className="p-2 rounded-md text-black"
             type="text"
             id="email"
             value={user.email}
@@ -40,7 +69,7 @@ export default function SignupPage() {
          />
          <label htmlFor="password">password</label>
          <input
-            className="p-1 rounded-lg"
+            className="p-2 rounded-md text-black"
             type="password"
             id="password"
             value={user.password}
@@ -51,14 +80,10 @@ export default function SignupPage() {
          <button
             className="border"
             onClick={onSignup}
-
-         >Signup here
+            
+         >{buttonDisabled ? "All fields required" : "Signup"}
          </button>
          <Link href="/login">Go to login page</Link>
-
-
-
-
 
 
       </div>
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -5,11 +5,15 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: [true, "Please provide a username"],
       unique: true,
+      trim: true,
+      lowercase: true
    },
    email: {
       type: String,
       required: [true, "Please provide an email"],
       unique: true,
+      trim: true,
+      lowercase: true
    },
    password: {
       type: String,
