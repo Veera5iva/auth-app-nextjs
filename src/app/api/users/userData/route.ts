f8db7ad6745bd1b3c053b80866fb76f29f4336dc
Veerasiva <veerasiva01@gmail.com>
2025-01-19 18:18:43 +0530

    add get user details function in the profile page

--- src/app/api/users/userData/route.ts
+++ src/app/api/users/userData/route.ts
@@ -10,11 +10,11 @@ connect();
 export async function GET(request: NextRequest) {
    try {
       const userID = await getDataFromToken(request);
-   const user = await User.findById(userID).select("-password");
-   return NextResponse.json({
-      message: "User data fetched successfully",
-      data: user
-   });
+      const user = await User.findById(userID).select("-password");
+      return NextResponse.json({
+         message: "User data fetched successfully",
+         data: user
+      });
       
    } catch (error: any) {
       return NextResponse.json({error: error.message}, {status: 500});
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 
 export default function ProfilePage() {
    const router = useRouter();
+   const [data, setData] = useState("nothing");
    const logout = async () => {
       try {
          await axios.get("/api/users/logout");
@@ -18,12 +21,28 @@ export default function ProfilePage() {
       }
 
    }
+
+   const getUserDetails = async () => {
+      const response = await axios.get("/api/users/userData");
+      console.log(response.data);
+      setData(response.data.data._id);
+
+   }
+
+
+
    return (
       <div className="flex flex-col items-center justify-center min-h-screen gap-y-1">
          <Toaster position="top-right" reverseOrder={false}/>
          <h1 className="text-3xl">Profile</h1>
          <hr />
          <p className="text-2xl">Profile page</p>
+         <h2>{data === "nothing" ? "No data" : <Link href={`/profile/${data}`}>Visit Profile</Link>}</h2>
+         <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={getUserDetails}
+         >Get user details</button>
+
          <hr />
          <button
             className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
